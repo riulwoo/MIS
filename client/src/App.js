3914ca995250b9ef8fe69c7beb83d777f46b6c54
mins <80028813+minseong00@users.noreply.github.com>
2022-09-14 07:54:17 +0000

    react test

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="App">
+       <div> 리액트로 만든 html 파일입니다 ~~~</div>
+     </div>
   );
 }
 
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.listen(4040, function() {
+  console.log('listening on 4000')
+})
