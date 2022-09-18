a57bcda0c4516b268eb74a928e514b6ad0ef0127
riulwoo <geun10121@naver.com>
2022-09-18 21:32:38 +0900

    220918 client build & server connect

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+const server = require('http').Server(app)
+const path = require('path');
+const io = require('socket.io')(server);
 
-app.listen(4040, function() {
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+})
+
+app.listen(4000, function() {
   console.log('listening on 4000')
 })
