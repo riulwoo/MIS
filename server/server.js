f52f499a162ed9116a4746159288ae8d2aeb79bf
mins <rkdalsrl20@gmail.com>
2022-10-03 21:47:02 +0900

    ...
    경수

--- server/server.js
+++ server/server.js
@@ -28,5 +28,6 @@ app.listen(PORT, function () {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public", "index.html"));
+  res.sendFile("/client/build/index.html", { root: ".." });
 });
+//path.join(__dirname, "../client/public", "index.html")
