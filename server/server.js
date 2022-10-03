8144cfc5eb0a4538d3ed30691cc81f6f23d13287
mins <rkdalsrl20@gmail.com>
2022-10-03 21:45:32 +0900

    ...
    경로수정

--- server/server.js
+++ server/server.js
@@ -28,5 +28,5 @@ app.listen(PORT, function () {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/public", "index.html"));
 });
