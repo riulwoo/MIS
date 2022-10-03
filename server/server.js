74199e07091e0f85e371691fc43a38d3f59b721a
mins <rkdalsrl20@gmail.com>
2022-10-03 20:52:36 +0900

    221003 folder
    1

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const path = require('path');
-const io = require('socket.io')(server);
-const connection = require('./DB/dbconn');
+const server = require("http").Server(app);
+const path = require("path");
+const io = require("socket.io")(server);
+const connection = require("./DB/dbconn");
 const PORT = 4000;
 
 // DB 사용법
@@ -17,31 +17,16 @@ const PORT = 4000;
 //   console.log(results);
 // })
 
-
 // 연결한 DB에 사용할 SQL 변수
 let sql = "";
 
 // React 프로젝트 폴더 연결
-app.use(express.static('client/build'));
+app.use(express.static("client/build"));
 
-app.listen(PORT, function() {
-  console.log(`listening on ${PORT}`)
-})
+app.listen(PORT, function () {
+  console.log(`listening on ${PORT}`);
+});
 
-app.get('/', (req, res)=>{
-    res.sendFile('/client/build/index.html', {root : '.'});
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", (req, res) => {
+  res.sendFile("/client/build/index.html", { root: ".." });
+});
